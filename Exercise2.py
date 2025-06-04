138110557da596ad5f45a1e4fd69d8a4468b62dc
Patriciah <triciah12@gmail.com>
2025-06-04 10:22:24 +0300

    .

---
+++ Exercise2.py
@@ -0,0 +1,44 @@
+
+print('hello world')
+print(5>3)
+print(3==3)
+print(3=='3')
+print(int(3)>3)
+print("hello"== "hello")
+computer_brand="Hp"
+print(computer_brand)
+print("I have a "+ computer_brand + "computer")
+name="patriciah"
+age=30
+shoe_size=39
+ 
+
+info=f"My name is{name},I am {age} years olds,my shoe size is{shoe_size}"
+print(info)
+
+a=8
+b=4
+if a >b:
+ print("hello world")
+
+user_input=input("enter number")
+number=int(user_input)
+if number%2==0:
+  print(number,"is odd")
+
+else:
+  print(number,"is even")
+
+my_name="patricia"
+name=input("enter name")
+if name==my_name:
+    print("Hey there namesake")
+else:
+ print("your name is not as beautiful as mine")
+
+height=input("enter your height in centimeters")
+height=int(height)
+if height>=145:
+ print("You are tall enough to ride")
+else:
+  print("You are too short.You need to grow more to ride")
